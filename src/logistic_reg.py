ccd0d42dab599941e56cd7c44341aff891f2bcae
Dane Jordan <drjordan@uw.edu>
2017-06-08 12:41:04 -0700

    update functions to read 'n' value

--- src/logistic_reg.py
+++ src/logistic_reg.py
@@ -23,6 +23,9 @@ def computeobj(beta, lamb, x, y):
     :param y: array
         labels
     """
+    
+    n = x.shape[0]
+
     obj = (1/n)*(np.sum(np.log(1 + np.exp(-y*np.dot(x, beta))))) \
         + lamb*np.sum(beta**2)
 
@@ -41,6 +44,9 @@ def computegrad(beta, lamb, x, y):
     :param y: array
         labels
     """
+
+    n = x.shape[0]
+
     grad_beta = -(1/n)*(np.dot(x.T, y/(np.exp(y*np.dot(x, beta)) + 1))) \
         + 2*lamb*beta
 
@@ -66,10 +72,12 @@ def backtracking(beta, lamb, x, y, eta=1, alpha=0.5, gamma=0.8, max_iter=100):
     :param max_iter: int
         stopping criterion
     """
+
     grad_beta = computegrad(beta, lamb, x, y)
     norm_grad_beta = np.sqrt(np.sum(grad_beta**2))
     found_eta = 0
     t = 0
+
     while found_eta == 0 and t < max_iter:
         if (computeobj(beta - eta*grad_beta, lamb, x, y) <
                 computeobj(beta, lamb, x, y) - alpha*eta*norm_grad_beta**2):
@@ -98,11 +106,14 @@ def fastgradalgo(beta_init, theta_init, lamb, x, y, max_iter):
     :param max_iter: int
         stopping criterion
     """
+
+    n = x.shape[0]
     beta = beta_init
     theta = theta_init
     eta_init = 1/(max(np.linalg.eigh(np.dot((1/n)*x.T, x))[0]) + lamb)
     beta_vals = [beta_init]
     t = 0
+
     while t < max_iter:
         eta = backtracking(beta, lamb, x, y, eta=eta_init)
         beta_next = theta - eta*computegrad(theta, lamb, x, y)
