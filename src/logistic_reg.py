4c030e0fff7936516bd2be5b1a46255e2aae6911
Dane Jordan <drjordan@uw.edu>
2017-06-08 11:51:12 -0700

    update docstrings and remove example code (moved to its own file)

--- src/logistic_reg.py
+++ src/logistic_reg.py
@@ -1,5 +1,8 @@
-import copy
-import matplotlib.pyplot as plt
+"""
+Minimizing Logistic Regression using Fast Gradient Algorithm with Backtracking
+"""
+# import copy # Currently not being used
+# import matplotlib.pyplot as plt # Currently not being used
 import numpy as np
 import pandas as pd
 
@@ -9,67 +12,107 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 InteractiveShell.ast_node_interactivity = "all"
-%matplotlib inline
+# %matplotlib inline # Commented out as it only pertains to .ipynb
+
 
 def computeobj(beta, lamb, x, y):
-    obj = (1/n)*(np.sum(np.log(1 + np.exp(-y*np.dot(x, beta))))) + lamb*np.sum(beta**2)
+    """
+    Compute a return a single value for the objective function
+    :param beta: array
+        initialize betas (typically zeros)
+    :param lamb: float
+        regularization parameter
+    :param x: array
+        data
+    :param y: array
+        labels
+    """
+    obj = (1/n)*(np.sum(np.log(1 + np.exp(-y*np.dot(x, beta))))) \
+        + lamb*np.sum(beta**2)
+
     return obj
 
+
 def computegrad(beta, lamb, x, y):
-    grad_beta = -(1/n)*(np.dot(x.T, y/(np.exp(y*np.dot(x, beta)) + 1))) + 2*lamb*beta
+    """
+    Compute the gradient of the objective function and return a vector size d
+    :param beta: array
+        initialize betas (typically zeros)
+    :param lamb: float
+        regularization parameter
+    :param x: array
+        data
+    :param y: array
+        labels
+    """
+    grad_beta = -(1/n)*(np.dot(x.T, y/(np.exp(y*np.dot(x, beta)) + 1))) \
+        + 2*lamb*beta
+
     return grad_beta
 
-def backtracking(beta, lamb, x, y, f=computeobj, g=computegrad, eta=1, alpha=0.5, gamma=0.8, max_iter=100):
-    grad_beta = g(beta, lamb, x, y)
+
+def backtracking(beta, lamb, x, y, eta=1, alpha=0.5, gamma=0.8, max_iter=100):
+    """
+    :param beta: array
+        initialize betas (typically zeros)
+    :param lamb: float
+        regularization parameter
+    :param x: array
+        data
+    :param y: array
+        labels
+    :param eta: float
+        initial step-size for backtracking
+    :param alpha: float
+        constant for sufficient decrease condition
+    :param gamma: float
+        constant for sufficient decrease condition
+    :param max_iter: int
+        stopping criterion
+    """
+    grad_beta = computegrad(beta, lamb, x, y)
     norm_grad_beta = np.sqrt(np.sum(grad_beta**2))
     found_eta = 0
     t = 0
-    while (found_eta == 0 and t < max_iter):
-        if (f(beta - eta*grad_beta, lamb, x, y) < f(beta, lamb, x, y) - alpha*eta*norm_grad_beta**2):
+    while found_eta == 0 and t < max_iter:
+        if (computeobj(beta - eta*grad_beta, lamb, x, y) <
+                computeobj(beta, lamb, x, y) - alpha*eta*norm_grad_beta**2):
             found_eta = 1
-        elif (t == max_iter):
+        elif t == max_iter:
             break
         else:
             eta = eta*gamma
             t += 1
+
     return eta
 
-def fastgradalgo(beta_init, theta_init, lamb, x, y, max_iter, f=computeobj, g=computegrad):
+
+def fastgradalgo(beta_init, theta_init, lamb, x, y, max_iter):
+    """
+    :param beta_init: array
+        initialize betas (typically zeros)
+    :param theta_init: array
+        initialize thetas (typically zeros)
+    :param lamb: float
+        regularization parameter
+    :param x: array
+        data
+    :param y: array
+        labels
+    :param max_iter: int
+        stopping criterion
+    """
     beta = beta_init
     theta = theta_init
-    eta_init = 1/(max(np.linalg.eigvals(np.dot((1/n)*x.T, x))) + lamb)
+    eta_init = 1/(max(np.linalg.eigh(np.dot((1/n)*x.T, x))[0]) + lamb)
     beta_vals = [beta_init]
     t = 0
-    while (t < max_iter):
+    while t < max_iter:
         eta = backtracking(beta, lamb, x, y, eta=eta_init)
-        beta_next = theta - eta*g(theta, lamb, x, y)
+        beta_next = theta - eta*computegrad(theta, lamb, x, y)
         theta = beta_next + t*(beta_next - beta)/(t + 3)
         beta = beta_next
         beta_vals.append(beta)
         t += 1
+
     return beta_vals
-
-spam = pd.read_table('https://statweb.stanford.edu/~tibs/ElemStatLearn/datasets/spam.data', sep=' ', header=None)
-spam = spam.dropna()
-test_indicator = pd.read_table('https://statweb.stanford.edu/~tibs/ElemStatLearn/datasets/spam.traintest', sep=' ', header=None)
-x = np.asarray(spam.drop(57, axis=1))
-y = np.asarray(spam[57])*2 - 1
-test_indicator = np.ravel(np.asarray(test_indicator))
-
-x_train = x[test_indicator == 0, :]
-x_test = x[test_indicator == 1, :]
-y_train = y[test_indicator == 0]
-y_test = y[test_indicator == 1]
-
-x_scaler = StandardScaler().fit(x_train)
-x_train = x_scaler.transform(x_train)
-x_test = x_scaler.transform(x_test)
-n = x_train.shape[0]
-d = x_train.shape[1]
-
-beta_init = np.zeros(d)
-theta_init = np.zeros(d)
-fastgradalgo(beta_init, theta_init, 0.1, x_train, y_train, 1000)[-1]
-
-logit = LogisticRegression(C=1/(2*n*0.1), fit_intercept=False, tol=1e-8).fit(x_train, y_train)
-logit.coef_
